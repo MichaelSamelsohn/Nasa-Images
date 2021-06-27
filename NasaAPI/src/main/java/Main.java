bc344e9564bd8c5591c56c58c18cacf1736dd8b9
MichaelSamelsohn <michael.samelsohn@gmail.com>
2021-06-27 12:05:32 +0300

    Added parametrized tests.

--- NasaAPI/src/main/java/Main.java
+++ NasaAPI/src/main/java/Main.java
@@ -1,17 +1,21 @@
 import NasaAPI.APOD.APOD;
+import NasaAPI.EPIC.EPIC;
 import NasaAPI.MarsRover.MarsRoverImages;
+import NasaAPI.NasaLibrary.ImageLibrary;
 
 public class Main {
     public static void main(String[] args) {
-        MarsRoverImages marsRoverImages = new MarsRoverImages();
-        marsRoverImages.getMarsRoverImages("Spirit", -4,"2022-12-23", "/Users/michaelsamelsohn/Desktop/");
+//        MarsRoverImages marsRoverImages = new MarsRoverImages();
+//        marsRoverImages.getMarsRoverImages("Spirit", 1,"2022-12-23", "/Users/michaelsamelsohn/Desktop/");
 
-//        EPIC epic = new EPIC();
-//        epic.getEpicImages(1, "/Users/michaelsamelsohn/Desktop/");
+        EPIC epic = new EPIC();
+        epic.getEpicImages(1, "/Users/michaelsamelsohn/Desktop/");
 
-//        getNasaLibraryImages(new String[]{"crab", "nebula"}, new String[]{"image"}, "1920", "2020");
+        ImageLibrary imageLibrary = new ImageLibrary();
+        imageLibrary.getNasaLibraryImages(new String[]{"crab", "nebula"}, new String[]{"image"}, "1920", "2020",
+                "/Users/michaelsamelsohn/Desktop/");
 
-//        APOD mwsa = new APOD();
-//        mwsa.getAstronomyPictureOfTheDay("2020-05-17",false,"/Users/michaelsamelsohn/Desktop/");
+        APOD apod = new APOD();
+        apod.getAstronomyPictureOfTheDay("2019-05-17",false,"/Users/michaelsamelsohn/Desktop/");
     }
 }
--- NasaAPI/src/main/java/NasaAPI/NasaLibrary/ImageLibrary.java
+++ NasaAPI/src/main/java/NasaAPI/NasaLibrary/ImageLibrary.java
@@ -3,10 +3,14 @@ package NasaAPI.NasaLibrary;
 import NasaAPI.Common;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import static java.lang.System.exit;
 
 public class ImageLibrary extends Common {
 
@@ -24,12 +28,12 @@ public class ImageLibrary extends Common {
         log.info("For full API documentation - https://images.nasa.gov/docs/images.nasa.gov_api_docs.pdf");
     }
 
-    private String[] getNasaLibraryDataUrl(String[] q, String[] mediaType, String startYear, String endYear) {
+    private String[] getNasaLibraryDataUrl(String[] query, String[] mediaType, String startYear, String endYear) {
         log.debug("The start year is - {}", startYear);
         log.debug("The end year is - {}", endYear);
 
         String[] photosUrl = new String[NUMBER_OF_PHOTOS_TO_COLLECT];
-        String urlComplement = "q=" + formatStringWithSeparator(q, "%20");
+        String urlComplement = "q=" + formatStringWithSeparator(query, "%20");
         log.debug("The URL complement before media type handling is - {}", urlComplement);
         if (mediaType != null) {
             urlComplement += "&" + "media_type=" + formatStringWithSeparator(mediaType, ",");
@@ -47,17 +51,17 @@ public class ImageLibrary extends Common {
             JSONObject collection = jsonObject.getJSONObject("collection");
             JSONArray items = collection.getJSONArray("items");
 
-            int j = 0;
-            for (int i = 0; (i < items.length()) && (j < NUMBER_OF_PHOTOS_TO_COLLECT); i++) {
+            for (int i = 0, j = 0; (i < items.length()) && (j < NUMBER_OF_PHOTOS_TO_COLLECT); i++, j++) {
                 JSONObject item = items.getJSONObject(i);
                 JSONArray links = item.getJSONArray("links");
                 JSONObject subItem = links.getJSONObject(0);
                 photosUrl[j] = subItem.getString("href");
                 log.debug("Added URL - {}", photosUrl[j]);
-                j++;
             }
-        } catch (Exception e) {
+        } catch (JSONException | UnirestException e) {
+            log.error("An error has occurred while conducting the manifest", e);
             e.printStackTrace();
+            exit(1);
         }
 
         return photosUrl;
